b5524ab2b6cd11197e531b61c95ba326ef2502c2
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-05-04 21:21:31 +0300

    Add files via upload

---
+++ WebApplication/Controllers/ApplicationController.cs
@@ -0,0 +1,32 @@
+﻿using Business.DTOs;
+using Business.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ApplicationController : ControllerBase
+    {
+        private readonly IApplicationService _applicationService;
+
+        public ApplicationController(IApplicationService applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var apps = await _applicationService.GetAllAsync();
+            return Ok(apps);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateApplicationRequest request)
+        {
+            var app = await _applicationService.CreateAsync(request);
+            return Ok(app);
+        }
+    }
+}
