d3a48c053428208ca32180a227bc69519524e62c
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-05-18 16:57:57 +0300

    Add files via upload

---
+++ WebApplication/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Net;
+using System.Text.Json;
+
+namespace WebApplication1.Middlewares
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+            var result = JsonSerializer.Serialize(new
+            {
+                message = exception.Message
+            });
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
